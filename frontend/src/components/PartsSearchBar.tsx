9a198ac5aa5eb52717de964a11063be1907a0573
Isaac Luo <yishaluo@gmail.com>
2019-02-11 16:47:00 +0000

    PartsSearchBar.tsx

---
+++ frontend/src/components/PartsSearchBar.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Input } from 'element-react';
+
+interface IProps {
+  text: string,
+  onSearch?: (searchKey: string) => void,
+}
+interface IState {
+  searchKey: string,
+}
+
+class PartsSearchBar extends React.Component<IProps, IState> {
+  constructor (props:IProps) {
+    super(props);
+    this.state = {
+      searchKey: props.text,
+    }
+  }
+
+  public componentWillReceiveProps(np: IProps) {
+    if(np.text !== this.props.text) {
+      this.setState({searchKey: np.text});
+    }
+  }
+
+  public render() {
+    return (
+      <Input
+        value={this.state.searchKey}
+        onChange={this.onSearchInputChange}
+        onKeyUp={this.onSearchInputKeyUp}
+      />
+    )
+  }
+
+  private onSearchInputChange = searchKey => {
+    this.setState({searchKey});
+  }
+
+  private onSearchInputKeyUp = event => {
+    if (event.keyCode === 13) {
+      if (this.props.onSearch) {
+        this.props.onSearch(this.state.searchKey);
+      }
+    }
+  };
+
+}
+
+export default PartsSearchBar;
