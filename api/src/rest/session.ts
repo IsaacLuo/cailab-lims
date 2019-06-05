d80e67c03307b9683179362683d8cc9808ae7fa7
Isaac Luo <yishaluo@gmail.com>
2019-06-05 23:26:00 +0100

    new api backend

---
+++ api/src/rest/session.ts
@@ -0,0 +1,56 @@
+import { 
+  ICustomState, 
+} from '../types';
+
+import koa from 'koa';
+import Router from 'koa-router';
+
+import {userMust, beAdmin, beUser} from '../identifyUsers'
+import { User, LogLogin } from '../models';
+import jwt from 'jsonwebtoken';
+import secret from '../../secret';
+
+export default function handleSessions (app:koa, router:Router) {
+
+  /**
+   * when user use a scanner and scan his barcode to login
+   * @body : {barcode}
+   */
+  router.post('/api/scannerSessions/',
+    async (ctx:koa.ParameterizedContext<ICustomState, {}>, next:()=>Promise<any>)=> {
+      const {barcode} = ctx.request.body;
+      if(!barcode) {
+        ctx.throw(406, 'barcode is required');
+      }
+      const user = await User.findOne({barcode}).exec();
+      if (!user) {
+        ctx.throw(406, 'invalid user');
+      }
+
+      const {_id, name, email, groups} = user;
+
+      if (groups.indexOf('scanner')<0) {
+        throw new Error('invalid user');
+      }
+      const token = jwt.sign({
+        _id,
+        fullName: name,
+        email,
+        groups:['scanner'], // user who only provide barcode get the scanner group only
+      }, 
+      secret.jwt.key,
+      {expiresIn:'1h'});
+      
+      LogLogin.create({
+        operator: _id,
+        operatorName: name,
+        type: 'login',
+        sourceIP: ctx.request.ip,
+        timeStamp: new Date(),
+      });
+      console.log(`scanner logged in as ${name}`);
+      ctx.body = {message: `welcome ${name} on scanner`, _id, token, name, email, groups:['scanner']};
+  });
+
+
+}
