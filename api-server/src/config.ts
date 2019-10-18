d2f5d73b3b53b6ca64450970bd389509bb99344f
Isaac Luo <yishaluo@gmail.com>
2019-10-18 20:09:51 +0100

    delete api-server and add yarn.lock

--- api-server/src/config.ts
+++
@@ -1,7 +0,0 @@
-import config from '../config.json'
-import { IGLobalConfig } from './types.js';
-
-const base = config.base;
-const diff = config[process.env.NODE_ENV];
-
-export default {...base, ...diff} as IGLobalConfig;
